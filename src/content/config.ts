cc1b2c7678210be9ff4460964bec3bdb4bdfc730
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2024-12-22 22:59:56 +1100

    add ctf showcase and team

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,38 @@ const showcase = defineCollection({
     }),
 });
 
+const people = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string().min(1),
+      image: image().optional(),
+      enrollments: z.array(z.object({
+        year: z.number().int().positive(),
+        role: z.string().min(1).optional()
+      }))
+    }),
+});
+
+const placementSchema = z.object({
+  year: z.number(),
+  teamName: z.string().optional(),
+  globalPlacement: z.number(),
+  australiaPlacement: z.number().optional(),
+  totalTeams: z.number().optional(),
+});
+
+export const ctfs = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    image: z.string().optional(),
+    placements: z.array(placementSchema),
+  }),
+});
+
 export const collections = {
   showcase,
+  people,
+  ctfs
 };
